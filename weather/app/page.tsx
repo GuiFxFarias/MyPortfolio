d42c3761d0e6c0d89eb53eff7bb01e44a76e8657
Guilherme Farias <109701085+GuiFxFarias@users.noreply.github.com>
2024-01-02 12:58:54 -0300

    commit

---
+++ weather/app/page.tsx
@@ -0,0 +1,11 @@
+import DateWeather from "@/components/date";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <DateWeather />
+      <h1>Hello World!!</h1>
+    </main>
+  );
+}
---
+++ weather/components/date.tsx
@@ -0,0 +1,7 @@
+export default function DateWeather() {
+  return (
+    <div>
+      <h1 className="text-black text-2xl">Data aqui</h1>
+    </div>
+  );
+}
